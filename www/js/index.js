efdd1195fb6b1cb77dd97fa6b9d4f894f494414d
Salvatore <salvatore.bruni@gmail.com>
2018-03-21 14:27:32 +0100

    142720180321

--- www/js/index.js
+++ www/js/index.js
@@ -95,31 +95,9 @@ var app = {
 		var context;
 		var image;
 		
-		// Esempio di Login
-		//localStorage.setItem("email","")
-		//localStorage.setItem("registrato", "2");
-	
-		
-		//Iphopne X
-		
-		osVersion = parseFloat(device.version);
-		
-		if(window.webkit && window.webkit.messageHandlers ) {
-			webView = "WKWebView" ;
-		}else{
-			webView = "UIWebView" ;
-		}
-		
-
-        app.setupPush();
-		
+				
 		var applaunchCount = localStorage.getItem("launchCount");
 		
-		/*document.addEventListener("touchmove",function(e) {
-		  e.preventDefault();
-		  },
-		  false
-		  );*/
 		
 		document.ontouchmove = function(e){ e.preventDefault(); }
 		
@@ -7146,58 +7124,8 @@ $(document).on("touchstart", "#editparty", function(e){
         }
 
 		
-    },
-    setupPush: function() {
-        console.log('calling push init');
-        var push = PushNotification.init({
-            "android": {
-                "senderID": "XXXXXXXX"
-            },
-            "browser": {},
-            "ios": {
-                "sound": true,
-                "vibration": true,
-                "badge": true,
-			    "clearBadge": true
-            },
-            "windows": {}
-        });
-        console.log('after init');
-
-            push.on('registration', function(data) {
-				
-            //console.log('registration event: ' + data.registrationId);
-			//testa(data.registrationId);
-
-            var oldRegId = localStorage.getItem('registrationId');
-            if (oldRegId !== data.registrationId) {
-                // Save new registration ID
-                localStorage.setItem('registrationId', data.registrationId);
-                // Post registrationId to your app server as the value has changed
-            }
-
-            var parentElement = document.getElementById('registration');
-            var listeningElement = parentElement.querySelector('.waiting');
-            var receivedElement = parentElement.querySelector('.received');
-
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
-        });
-
-        push.on('error', function(e) {
-            console.log("push error = " + e.message);
-        });
-
-        push.on('notification', function(data) {
-            console.log('notification event');
-            navigator.notification.alert(
-                data.message,         // message
-                null,                 // callback
-                data.title,           // title
-                'Ok'                  // buttonName
-            );
-       });
     }
+    
 };
 
 
